757ced330d748920ede9ffd4686a8a23c14bf522
Amit Rawal <atitrawal4505@gmail.com>
2024-12-30 00:03:00 +0545

    complete update Code

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,10 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 import transporter from "../config/nodemailer.js";
+import {
+  EMAIL_VERIFY_TEMPLATE,
+  PASSWORD_RESET_TEMPLATE,
+} from "../config/emailTemplete.js";
 
 //Register User
 
@@ -124,7 +128,11 @@ export const sendVerifyOtp = async (req, res) => {
       from: process.env.SENDER_EMAIL,
       to: user.email,
       subject: "Account Verification OTP",
-      text: `Your OTP is ${otp}.Verify your account using this OTP.`,
+      // text: `Your OTP is ${otp}.Verify your account using this OTP.`,
+      html: EMAIL_VERIFY_TEMPLATE.replace("{{otp}}", otp).replace(
+        "{{email}}",
+        user.email
+      ),
     };
     await transporter.sendMail(mailOption);
     return res.json({
@@ -205,7 +213,11 @@ export const sendResetOtp = async (req, res) => {
       from: process.env.SENDER_EMAIL,
       to: email,
       subject: "Password Reset OTP",
-      text: `Your OTP for resetting your password is ${otp}.Use this OTP to proceed with resetting your password.`,
+      // text: `Your OTP for resetting your password is ${otp}.Use this OTP to proceed with resetting your password.`,
+      html: PASSWORD_RESET_TEMPLATE.replace("{{otp}}", otp).replace(
+        "{{email}}",
+        user.email
+      ),
     };
 
     await transporter.sendMail(mailOption);
@@ -221,12 +233,10 @@ export const resetPassword = async (req, res) => {
   const { email, otp, newPassword } = req.body;
 
   if (!email || !otp || !newPassword) {
-    return res
-      .status(400)
-      .json({
-        success: false,
-        message: "Please provide all the required fields",
-      });
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all the required fields",
+    });
   }
 
   try {
